7143b92ab6d2e1f0eed4baea7a0cc71665ce9186
jpllosa <jpllosa@yahoo.com>
2015-12-14 16:38:14 +0800

    added Router object

--- src/main/java/example/LogService.java
+++ src/main/java/example/LogService.java
@@ -3,6 +3,7 @@ package example;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
 
 public class LogService {
 
@@ -11,20 +12,21 @@ public class LogService {
 		
 		HttpServer server = vertx.createHttpServer();
 	
-		server.requestHandler(request -> {
+		Router router = Router.router(vertx);
+		
+		router.route("/log").handler(routingContext -> {
 			
-			String mParamValue = request.getParam("m");
-			String sParamValue = request.getParam("s");
+			String mParamValue = routingContext.request().getParam("m");
+			String sParamValue = routingContext.request().getParam("s");
 			if ((mParamValue != null) && (sParamValue != null)) {
 				System.out.println("message: " + mParamValue + ", stacktrace: " + sParamValue);
 			}
 
-			HttpServerResponse response = request.response();
+			HttpServerResponse response = routingContext.response();
 			response.end();
 		});
 	
-		server.listen(8080);
+		server.requestHandler(router::accept).listen(8080);
 		System.out.println("Vert.x HTTP server listening at port 8080...");
-	}
-
+	}	
 }
